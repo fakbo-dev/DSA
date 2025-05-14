e011b7e787a028c4e98dd969becbc633e8dae2e8
fakbito-dev <fakbitodev@gmail.com>
2025-05-13 22:03:58 -0400

    getting starter

--- 01-fundamentals/main.js
+++ 01-fundamentals/main.js
@@ -1,2 +1,18 @@
-const gettingStarted = 'Hello World';
-console.log(gettingStarted);
+let baseSalary = 30000;
+let overtime = 10;
+let rate = 20;
+
+function getWage(baseSalary,overtime,rate) {
+  return baseSalary * overtime * rate;
+}
+
+const employee = {
+  baseSalary: 30000,
+  overtime: 10,
+  rate: 20,
+  getWage: function() {
+    return this.baseSalary + this.overtime + this.rate;
+  }
+};
+
+console.log(employee.getWage());
