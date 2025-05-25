e77a459b9cf421437c0f16172c4f2da3d2d70682
fakbito-dev <fakbitodev@gmail.com>
2025-05-25 18:36:19 -0400

    i undertanding now

--- 01-fundamentals/Arrays/leet/02-removeDuplicates.js
+++ 01-fundamentals/Arrays/leet/02-removeDuplicates.js
@@ -1,9 +1,10 @@
-//Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once.
-//The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+//Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once
+//The relative order of the elements should be kept the same. Then return the number of unique elements in nums
 
 //Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
 
-//    Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+//    Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially.
+//    The remaining elements of nums are not mportant as well as the size of nums.
 //    Return k.
 
 //Custom Judge:
@@ -39,16 +40,16 @@
 //It does not matter what you leave beyond the returned k (hence they are underscores).
 
 function removeDuplicates(nums) {
-let i = 1;
-let j = 1;
+  let i = 1;
+  let j = 1;
 
-  while (i < nums.length) {
+  while (i <= nums.length -1) {
 
     if (nums[i] !== nums[i - 1]) {
       nums[j] = nums[i]
       j++
     }
-    i++
+    i++;
   }
   return j;
 }
