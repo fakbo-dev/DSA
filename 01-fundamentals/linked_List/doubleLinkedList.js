6916a8aa1fbff753c59855d0d24a74a451e343e4
fakbo-dev <fakbo0206@gmail.com>
2025-06-08 10:09:05 -0400

    done with insertion

--- 01-fundamentals/linked_List/doubleLinkedList.js
+++ 01-fundamentals/linked_List/doubleLinkedList.js
@@ -38,6 +38,7 @@ class DoublyLinkedList {
         return arr;
     }
     reverseTraversal() {
+        if (this.head === null)  return [];
         let current = this.head;
         const arr = [];
         while (current.next !== null) {
@@ -49,6 +50,56 @@ class DoublyLinkedList {
         }
         return arr;
     }
+    length() {
+        let length = 0;
+        if (this.head === null) return 0;
+        let current = this.head;
+
+        while (current !== null) {
+        length++;
+            current = current.next
+        }
+        return length;
+    }
+
+    unshift(data) {
+        const newNode = new Node(data);
+        if (this.head === null) {
+            this.head = newNode;
+            return newNode;
+        }
+        this.head.prev = newNode;
+        newNode.next = this.head;
+        this.head = newNode;
+        return newNode;
+    }
+    insert(index,data) {
+        if (index < 0 ) return 'the index is lower than the first node'
+        if (index === 0) {this.unshift(data); return;}
+        if (index === this.length()) {this.push(data); return;}
+        if (index > this.length()) return 'the index is greater than the List'
+        const newNode = new Node(data)
+        let length = 0;;
+        let current = this.head;
+
+        while (current !== null) {
+
+            if (length + 1 === index) {
+                let temp = current.next;
+                current.next = newNode;
+                newNode.prev = current;
+                if (temp !== null) {
+                newNode.next = temp;
+                temp.prev = newNode;
+                }
+                return newNode;
+            }
+
+            length++
+            current = current.next;
+        }
+        return newNode;
+    }
 }
 
 
@@ -57,5 +108,9 @@ myDoublyLinkedList.push(1);
 myDoublyLinkedList.push(2);
 myDoublyLinkedList.push(3);
 myDoublyLinkedList.push(4);
-console.log(myDoublyLinkedList.traversal());
-console.log(myDoublyLinkedList.reverseTraversal());
+//console.log(myDoublyLinkedList.traversal());
+//console.log(myDoublyLinkedList.reverseTraversal());
+//console.log(myDoublyLinkedList.length());
+myDoublyLinkedList.unshift(0);
+//console.log(myDoublyLinkedList);
+console.log(myDoublyLinkedList.insert(2,'minus'));
