6e0532ed512022fbbdaff41e274af085406c7769
fakbo-dev <fakbo0206@gmail.com>
2025-06-15 20:26:33 -0400

    something

---
+++ 01-fundamentals/linked_List/leet/megerTwoSortedList.js
@@ -0,0 +1,11 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+function mergeTwoLists(list1,list2) {
+
+};
+mergeTwoLists([1,2,4],[1,3,4])
