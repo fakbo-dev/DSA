bad38e7be2059fd5f94c41a5334ee3358de72cb3
fakbo-dev <fakbodev@gmail.com>
2025-06-23 21:33:02 -0400

    dont care is working

--- 02-algorithms/sorting/bubbleSort.js
+++ 02-algorithms/sorting/bubbleSort.js
@@ -1,29 +1,26 @@
-
 function bubbleSort(arr) {
-
   let i = 0;
   let j = 0;
   let temp;
-
   while (i <= arr.length - 1) {
-
+    let sorted = false;
     while (j <= arr.length - 1) {
     if (arr[j] > arr[j + 1] && arr[j + 1] !== undefined) {
       temp = arr[j];
       arr[j] = arr[j + 1];
       arr[j + 1] =  temp
+        sorted = true
     }
-
       j++
     }
-
+    if (!sorted) return arr;
     j = 0
     i++;
+    console.log(`run Number: ${i}`)
   }
   return arr;
 }
 
-
-
-
 console.log(bubbleSort([7,12,9,11,3]));
+console.log(bubbleSort([100, 50, 75, 25, 1000, 1, 0, -1]));
+console.log(bubbleSort([7, 3, 9, 12, 11]));
