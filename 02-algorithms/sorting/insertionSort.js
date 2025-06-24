9c731688b755e4f9201df77dafcd1df09992857b
fakbo-dev <fakbodev@gmail.com>
2025-06-24 15:36:01 -0400

    adding some

--- 02-algorithms/sorting/insertionSort.js
+++ 02-algorithms/sorting/insertionSort.js
@@ -4,14 +4,13 @@
 //  go to the right if the value is greater than the check value
 //  repeat until there's no more value lefts
 function insertionSort(arr) {
-// 1. make the outer loop 
   for (let i = 1; i <= arr.length - 1; i++) {
     let temp = arr[i];
     let j = i - 1;
 
     while (j >= 0 && temp < arr[j]) {
       arr[j + 1] = arr[j];
-      j--;
+      j--
     }
 
     arr[j + 1] = temp;
