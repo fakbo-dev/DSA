ff16c80ee389f4d4cc5297b0fed448acc9877c41
fakbo-dev <fakbodev@gmail.com>
2025-06-23 20:58:12 -0400

    i am a genius

--- 02-algorithms/sorting/bubbleSort.js
+++ 02-algorithms/sorting/bubbleSort.js
@@ -2,19 +2,24 @@
 function bubbleSort(arr) {
 
   let i = 0;
+  let j = 0;
   let temp;
+
   while (i <= arr.length - 1) {
-    // need to take the first two Values and evaluate
-    //    is the first value greater than the second Value?, if that so swap.
-    //    if not keep going
-    if (arr[i] > arr[i + 1] && arr[i + 1] !== undefined) {
-      temp = arr[i];
-      arr[i] = arr[i + 1];
-      arr[i + 1] =  temp
+
+    while (j <= arr.length - 1) {
+    if (arr[j] > arr[j + 1] && arr[j + 1] !== undefined) {
+      temp = arr[j];
+      arr[j] = arr[j + 1];
+      arr[j + 1] =  temp
     }
+
+      j++
+    }
+
+    j = 0
     i++;
   }
-
   return arr;
 }
 
@@ -22,3 +27,15 @@ function bubbleSort(arr) {
 
 
 console.log(bubbleSort([7,12,9,11,3]));
+
+
+  //while (i <= arr.length - 1) {
+  //  // i need to iterate over the array n times
+  //  // how do i do that?
+  //  if (arr[i] > arr[i + 1] && arr[i + 1] !== undefined) {
+  //    temp = arr[i];
+  //    arr[i] = arr[i + 1];
+  //    arr[i + 1] =  temp
+  //  }
+  //  i++;
+  //}
