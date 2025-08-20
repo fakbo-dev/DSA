adcab56bf005a3cbb1fb2282a8e2cf0c2828b3c6
fakbo-dev <fakbodev@gmail.com>
2025-08-19 21:33:02 -0400

    feat:some

--- 03-concepts/recursion.js
+++ 03-concepts/recursion.js
@@ -1,16 +1,16 @@
-function sum(int)
+function sum(n)
 {
-  let res = 0;
-  let i = 0;
-  while (i <= int)
-  {
-    res += i;
-    i++;
-  }
+  if (n === 1) return 1;
 
-  return res
+  return n + sum(n - 1);
 }
+// the factorial of a number is the multiply of n between 1 and n
+function fact(n)
+{
+  if (n === 0) return 1;
 
-
-console.log(sum(5));
+  return n * fact(n - 1);
+}
+console.log('the sum of n number between 5 and 0 is',sum(5));
+console.log('the factorial of the number 5 is',fact(5));
 
