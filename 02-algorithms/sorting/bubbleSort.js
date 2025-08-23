5697cea38dff73b1cde7adf12de13ec6798a0980
fakbo-dev <fakbodev@gmail.com>
2025-08-23 14:52:52 -0400

    feat: proud of me

--- 02-algorithms/sorting/bubbleSort.js
+++ 02-algorithms/sorting/bubbleSort.js
@@ -1,26 +1,30 @@
-function bubbleSort(arr) {
+function bubbleSort(arr)
+{
   let i = 0;
-  let j = 0;
-  let temp;
-  while (i <= arr.length - 1) {
+  const arrLength = arr.length - 1
+
+  while (i <= arrLength)
+  {
+    let j = 1;
+    let temp;
     let sorted = false;
-    while (j <= arr.length - 1) {
-    if (arr[j] > arr[j + 1] && arr[j + 1] !== undefined) {
-      temp = arr[j];
-      arr[j] = arr[j + 1];
-      arr[j + 1] =  temp
-        sorted = true
-    }
-      j++
+    while (j <= arrLength)
+    {
+      if (arr[j - 1] > arr[j])
+      {
+        temp = arr[j];
+        arr[j] = arr[j - 1];
+        arr[j - 1] = temp;
+        sorted = true;
+      }
+      j++;
     }
     if (!sorted) return arr;
-    j = 0
     i++;
-    console.log(`run Number: ${i}`)
   }
   return arr;
 }
 
-console.log(bubbleSort([7,12,9,11,3]));
+// console.log(bubbleSort([7,12,9,11,3]));
 console.log(bubbleSort([100, 50, 75, 25, 1000, 1, 0, -1]));
-console.log(bubbleSort([7, 3, 9, 12, 11]));
+// console.log(bubbleSort([7, 3, 9, 12, 11]));
