fc1d5fae27463ac7662b9533643b8640c4e252f7
fakbo-dev <fakbodev@gmail.com>
2025-08-28 21:23:09 -0400

    im gonna finish tomorrow

--- 02-algorithms/sorting/quickSort.js
+++ 02-algorithms/sorting/quickSort.js
@@ -1,5 +1,23 @@
 function quickSort(arr)
 {
+    const pivot = arr[arr.length - 1];
+    let j = 0;
+    let i = -1;
+    console.log(arr[j]);
+    while (true)
+    {
+        while (arr[j] > pivot)
+        {
+            j++;
+        }
+        console.log(arr[j]);
+        while (arr[i] < pivot)
+        {
+            i++;
+        }
+        [arr[j],arr[i]] = [arr[i],arr[j]];
+    }
+    console.log(arr);
 }
 
-console.log(quickSort('testing2'));
+quickSort([316,933,337,671,21,35,966,495]);
