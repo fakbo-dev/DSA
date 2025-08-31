8e6e56b1983c2ef77b8510fe0ca2294f8db439dd
fakbo-dev <fakbodev@gmail.com>
2025-08-31 15:45:44 -0400

    some

--- 02-algorithms/sorting/quickSort.js
+++ 02-algorithms/sorting/quickSort.js
@@ -1,5 +1,6 @@
 function quickSort(arr)
 {
+    if (arr.length === 1) return arr;
     const pivot = Math.floor((arr.length - 1) / 2);
     [arr[pivot],arr[arr.length -1]] = [arr[arr.length - 1], arr[pivot]]
 
@@ -19,6 +20,9 @@ function quickSort(arr)
         [arr[i],arr[j]] = [arr[j],arr[i]];
     }
     [arr[i],arr[arr.length - 1]] = [arr[arr.length - 1], arr[i]]
+    const left = arr.slice(0,i - 1);
+    const right = arr.slice(i +1);
+    quickSort(left);
 }
 
 quickSort([316,933,337,671,21,35,966,495]);
