fc8f2b7820e4f3da49c16baa46c9ee0bbb0cef39
fakbo-dev <fakbodev@gmail.com>
2025-09-30 22:11:24 -0400

    finish DFS in DSA

--- 01-fundamentals/Tree/binaryTree.js
+++ 01-fundamentals/Tree/binaryTree.js
@@ -15,29 +15,46 @@ class BinaryTree
     this.root = null
   }
 
-  insert(data)
+  inOrder(node = this.root)
   {
-    const newNode = new Node(data);
-    if (this.root === null) return this.root = newNode;
-    let temp = this.root
-
-    while (true)
-    {
-      if (temp.left === null)
-      {
-        return temp.left = newNode;
-      }
-      else if (temp.right === null)
-      {
-        return temp.right = newNode;
-      }
-    }
+    if (node === null) return;
+    this.inOrder(node.left);
+    console.log(node.data);
+    this.inOrder(node.right);
+  }
+  preOrder(node = this.root)
+  {
+    if (node === null) return;
+    console.log(node.data);
+    this.preOrder(node.left);
+    this.preOrder(node.right);
+  }
+  postOrder(node = this.root)
+  {
+    if (node === null) return;
+    this.postOrder(node.left);
+    this.postOrder(node.right);
+    console.log(node.data);
   }
 }
 
+
 const tree = new BinaryTree();
-tree.insert(89);
-tree.insert(52);
-tree.insert(98);
-tree.insert(87);
-console.log("tree = ", tree)
+tree.root = new Node(89);
+tree.root.left = new Node(52);
+tree.root.right = new Node(98);
+tree.root.left.left = new Node(87);
+tree.root.left.right = new Node(51);
+tree.root.right.left = new Node(15);
+tree.root.right.right = new Node(60);
+tree.root.left.left.left = new Node(88);
+tree.root.left.left.right = new Node(90);
+tree.root.left.right.left = new Node(59);
+tree.root.left.right.right = new Node(97);
+tree.root.right.left.left = new Node(69);
+tree.root.right.left.right = new Node(52);
+tree.root.right.right.left = new Node(19);
+tree.root.right.right.right = new Node(59);
+console.log(tree.preOrder());
+console.log(tree.inOrder());
+console.log(tree.postOrder());
