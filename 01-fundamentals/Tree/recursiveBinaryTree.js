7140e6ce030f3d5855df7d4514689610ea532a61
fakbo-dev <fakbodev@gmail.com>
2025-10-06 20:24:39 -0400

    lock in

--- 01-fundamentals/Tree/recursiveBinaryTree.js
+++ 01-fundamentals/Tree/recursiveBinaryTree.js
@@ -1,12 +1,14 @@
 class Node
 {
-  constructor(data)
+  constructor(value)
   {
-    this.data = data
+    this.value = value
     this.left = null
     this.right = null
   }
 }
+
+
 
 class BinaryTree
 {
@@ -15,51 +17,51 @@ class BinaryTree
     this.root = null
   }
 
-  preOrder(node = this.root)
+  BFSHANDLER(node,level,queue)
   {
+    //base case
     if (node === null) return;
-    console.log(node.data);
-    this.preOrder(node.left);
-    this.preOrder(node.right);
-  }
 
-  inOrder(node = this.root)
-  {
-    if (node === null) return;
-    this.inOrder(node.left);
-    console.log(node.data);
-    this.inOrder(node.right);
+    //2. check if there a new level if so push an level separator
+    if (queue.length <= level) queue.push([]);
+
+    //3. push my values to my level separator
+    queue[level].push(node.value);
+    // 4. check my left and right nodes
+    this.BFSHANDLER(node.left,level + 1, queue);
+    this.BFSHANDLER(node.right,level + 1, queue);
   }
-  postOrder(node = this.root)
+  BFS()
   {
-    if (node === null) return;
-    this.postOrder(node.left);
-    this.postOrder(node.right);
-    console.log(node.data);
+    const res = [];
+
+    this.BFSHANDLER(this.root,0,res);
+    return res;
   }
   getLevel()
   {
-
+    const response = this.BFS();
+    return response.length - 1;
   }
 }
 
-
 const tree = new BinaryTree();
-tree.root = new Node(89);
-tree.root.left = new Node(52);
-tree.root.right = new Node(98);
-tree.root.left.left = new Node(87);
-tree.root.left.right = new Node(51);
-tree.root.right.left = new Node(15);
-tree.root.right.right = new Node(60);
-tree.root.left.left.left = new Node(88);
-tree.root.left.left.right = new Node(90);
-tree.root.left.right.left = new Node(59);
-tree.root.left.right.right = new Node(97);
-tree.root.right.left.left = new Node(69);
-tree.root.right.left.right = new Node(52);
-tree.root.right.right.left = new Node(19);
-tree.root.right.right.right = new Node(59);
-console.log(tree.preOrder());
-// console.log(tree.inOrder());
-// console.log(tree.postOrder());
+
+//Create the Nodes
+const firstNode = new Node(19);
+const secondNode  = new Node(9);
+const thirdNode = new Node(16);
+const fourthNode = new Node(24);
+const fifthNode = new Node(75);
+const sixthNode = new Node(17);
+
+// link the binary Tree
+
+tree.root = firstNode;
+tree.root.left = secondNode;
+tree.root.right = thirdNode;
+tree.root.left.left = fourthNode;
+tree.root.left.right = fifthNode;
+tree.root.right.left = sixthNode;
+console.log(tree.BFS());
+console.log(tree.getLevel());
