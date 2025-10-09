342ce39ca98323370fc3d2a37904cdcf9e02cbca
fakbo-dev <fakbodev@gmail.com>
2025-10-08 20:16:39 -0400

    some

--- 01-fundamentals/Tree/iterativeBinaryTree.js
+++ 01-fundamentals/Tree/iterativeBinaryTree.js
@@ -17,16 +17,75 @@ class Tree
 
   BFS()
   {
+    if (this.root === null) return [];
+
     const q = [];
-    const result = [];
-    let current = this.root;
-    q.push(current.value);
+    const response = [];
+    let currentLevel = 0;
+    q.push(this.root);
+
     while (q.length > 0)
     {
-      result.push(q.shift());
-      q.push(current.left.value);
-      q.push(current.right.value);
+      response.push([]);
+      let i = 0;
+      const len = q.length - 1;
+      while (i <= len)
+      {
+        let node = q.shift();
+        response[currentLevel].push(node.value);
+        if ( node.left !== null) q.push(node.left);
+        if ( node.right !== null) q.push(node.right);
+        i++;
+      }
+      currentLevel++;
     }
+
+    return response;
+  }
+
+  getLevel()
+  {
+    const res = this.BFS();
+
+    return res.length -1;
+  }
+
+  getNodeCount()
+  {
+    const res = this.BFS();
+    let count = 0;
+    let i = 0;
+
+    while (i <= res.length - 1)
+    {
+      let j = 0;
+
+      while (j <= res[i].length -1)
+      {
+        count++;
+        j++;
+      }
+      i++
+    }
+    return count;
+  }
+  // root -> left -> right
+  preOrder()
+  {
+    const response = [];
+    if (this.root === null) return response;
+    const stack = []
+    stack.push(this.root);
+
+    while (stack.length > 0)
+    {
+      const node = stack.pop();
+      response.push(node.value);
+
+      if (node.right !== null) stack.push(node.right);
+      if (node.left !== null) stack.push(node.left);
+    }
+    return response;
   }
 }
 
@@ -45,4 +104,7 @@ tree.root.right.left.right = new Node(3);
 tree.root.right.right = new Node(2);
 tree.root.right.right.left = new Node(8);
 tree.root.right.right.right = new Node(11);
-console.log(tree.BFS());
+// console.log(tree.BFS())
+// console.log(tree.getLevel());
+// console.log(tree.getNodeCount());
+console.log(tree.preOrder());
