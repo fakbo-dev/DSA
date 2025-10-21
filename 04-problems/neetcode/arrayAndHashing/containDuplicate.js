baf70bdcf5f0a1e461ba46340b30de1460971ed4
fakbo-dev <fakbodev@gmail.com>
2025-10-20 20:11:01 -0400

    first problem solve

--- 04-problems/neetcode/arrayAndHashing/containDuplicate.js
+++ 04-problems/neetcode/arrayAndHashing/containDuplicate.js
@@ -1,7 +1,48 @@
-function hasDuplicate(nums)
-{
+//default way O(n)
 
-}
+// function hasDuplicate(nums)
+// {
+//     const h = new Map();
+//     console.log(h);
+//     let i = 0;
+//     while (i <= nums.length - 1)
+//     {
+//         if (!h.has(nums[i]))
+//         {
+//             h.set(nums[i],0);
+//         } else
+//         {
+//             return true;
+//         }
+//         i++;
+//     }
+//     return false;
+// }
 
 
-console.log(hasDuplicate([1,2,3,3]));
+// console.log(hasDuplicate([1,2,3,3]));
+
+//worst solution O(n)^2
+
+// function hasDuplicate(nums)
+// {
+//
+//     const check = [];
+//     let i = 0;
+//     while (i <= nums.length - 1)
+//     {
+//         let checkNumber = nums[i];
+//         let j = i + 1;
+//         while (j <= nums.length - 1)
+//         {
+//             if (nums[j] === checkNumber) return true;
+//             if (j === nums.length - 1) check.push(nums[j]);
+//             j++;
+//         }
+//         i++;
+//     }
+//     return false;
+// }
+
+// console.log(hasDuplicate([1,2,3,3]));
+
