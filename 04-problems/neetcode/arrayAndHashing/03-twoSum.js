f2b8127e5a742b52f272586da3fd0144d6f21141
fakbo-dev <fakbodev@gmail.com>
2025-10-28 21:05:35 -0400

    made it but i need to understand why

--- 04-problems/neetcode/arrayAndHashing/03-twoSum.js
+++ 04-problems/neetcode/arrayAndHashing/03-twoSum.js
@@ -28,21 +28,33 @@
 //sorted approach bet better i guess? two pointers
 function twoSum(nums,target)
 {
-    if (nums.length === 2) return [0,1];
-    const sorted = nums.toSorted((a,b) => b - a);
+    const shallowCopy = [];
+
+    for (let i = 0; i <= nums.length - 1; i++)
+    {
+        shallowCopy.push([nums[i],i]);
+    }
     let i = 0;
     let j = nums.length - 1;
+    shallowCopy.sort((a,b) => a[0] - b[0]);
     while (i < j)
     {
-        let total = sorted[i] + sorted[j];
-        if (Math.abs(total) === Math.abs(target)) break;
-        if (Math.abs(total) > Math.abs(target)) j--;
-        if (Math.abs(total) < Math.abs(target)) i++;
+        let sum = shallowCopy[i][0] + shallowCopy[j][0];
+
+        if (sum === target) return [Math.min(shallowCopy[i][1],shallowCopy[j][1]),Math.max(shallowCopy[i][1],shallowCopy[j][1])];
+        if (sum < target) i++;
+        if (sum > target) j--;
     }
-    const finisha = sorted.findIndex((element) => element === sorted[i]);
-    const finishb = sorted.findIndex((element) => element === sorted[j]);
-    return [finisha,finishb];
 }
 
-console.log(twoSum([2,5,5,11],10));
-
+console.log(twoSum([-1,-2,-3,-4,-5],-8));
+//clever move!
+function testing(nums)
+{
+        let A = [];
+        for (let i = 0; i < nums.length; i++) {
+            A.push([nums[i], i]);
+        }
+    console.log(A);
+}
+// console.log(testing([2,5,5,11]));
