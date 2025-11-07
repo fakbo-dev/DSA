526864ffb1059831cca900b2492c4b170f39c20c
fakbo-dev <fakbodev@gmail.com>
2025-11-06 21:45:07 -0400

    some

--- 04-problems/leetcode/sql/04-averageSellingPrice.sql
+++ 04-problems/leetcode/sql/04-averageSellingPrice.sql
@@ -1,6 +1,12 @@
 SELECT
   pri.product_id,
-  ROUND(SUM(CAST(uni.units AS FLOAT) * CAST(pri.price as FLOAT) / SUM(CAST(uni.units as FLOAT), 2) as average_price
+  ISNULL (
+    ROUND(
+      SUM((CAST(units as FLOAT) * CAST(price AS FLOAT))) / SUM(CAST(units AS FLOAT)),
+      2
+    ),
+    0
+  ) as average_price
 FROM
   Prices as pri
   LEFT JOIN UnitsSold uni ON pri.product_id = uni.product_id
