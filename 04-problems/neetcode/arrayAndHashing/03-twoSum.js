5610c8619d5fa2fbd72493de45f1b93ef1d205a8
fakbo-dev <fakbodev@gmail.com>
2025-12-06 19:32:22 -0400

    testing

--- 04-problems/neetcode/arrayAndHashing/03-twoSum.js
+++ 04-problems/neetcode/arrayAndHashing/03-twoSum.js
@@ -28,42 +28,42 @@
 // sorted approach bet better i guess? two pointers
 function twoSum(nums, target)
 {
-    const shallowCopy = [];
+  const shallowCopy = [];
 
-    for (let i = 0; i <= nums.length - 1; i++)
-    {
-        shallowCopy.push([ nums[i], i ]);
-    }
-    let i = 0;
-    let j = nums.length - 1;
-    shallowCopy.sort((a, b) => a[0] - b[0]);
-    while (i < j)
-    {
-        let sum = shallowCopy[i][0] + shallowCopy[j][0];
+  for (let i = 0; i <= nums.length - 1; i++)
+  {
+    shallowCopy.push([ nums[i], i ]);
+  }
+  let i = 0;
+  let j = nums.length - 1;
+  shallowCopy.sort((a, b) => a[0] - b[0]);
+  while (i < j)
+  {
+    let sum = shallowCopy[i][0] + shallowCopy[j][0];
 
-        if (sum === target)
-        {
-            return [
-                Math.min(shallowCopy[i][1], shallowCopy[j][1]),
-                Math.max(shallowCopy[i][1], shallowCopy[j][1])
-            ];
-        }
-        if (sum < target)
-            i++;
-        if (sum > target)
-            j--;
+    if (sum === target)
+    {
+      return [
+        Math.min(shallowCopy[i][1], shallowCopy[j][1]),
+        Math.max(shallowCopy[i][1], shallowCopy[j][1])
+      ];
     }
+    if (sum < target)
+      i++;
+    if (sum > target)
+      j--;
+  }
 }
 
 console.log(twoSum([ -1, -2, -3, -4, -5 ], -8));
 // clever move! using hashmap
 function testing(nums)
 {
-    let A = [];
-    for (let i = 0; i < nums.length; i++)
-    {
-        A.push([ nums[i], i ]);
-    }
-    console.log(A);
+  let A = [];
+  for (let i = 0; i < nums.length; i++)
+  {
+    A.push([ nums[i], i ]);
+  }
+  console.log(A);
 }
 // console.log(testing([2,5,5,11]));
