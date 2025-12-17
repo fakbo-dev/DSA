add78f22876cbd5ed5eff6cb468ec27f2efc3837
fakbodev <fakbodev@gmail.com>
2025-12-17 14:50:19 -0400

    add: finish with the solutions of contains Duplicate

--- 04-problems/leetcode/typescript/07-containsDuplicate.ts
+++ 04-problems/leetcode/typescript/07-containsDuplicate.ts
@@ -1,26 +1,75 @@
 /*
 TODO: make it with 3 ways
 1. Brute Force
-2. sorted
+2. sorted (done)
 3. Hashing (done)
 * */
-function containsDuplicate(nums: number[]): boolean
-{
-  const map: Map<number, number> = new Map();
 
-  for (let i: number = 0; i <= nums.length - 1; i++)
-  {
-    if (map.has(nums[i]) && map.get(nums[i]) !== i)
-    {
-      return true
-    }
+// HashMap
+// function containsDuplicate(nums: number[]): boolean
+// {
+//   const map: Map<number, number> = new Map();
+//
+//   for (let i: number = 0; i <= nums.length - 1; i++)
+//   {
+//     if (map.has(nums[i]) && map.get(nums[i]) !== i)
+//     {
+//       return true
+//     }
+//
+//     map.set(nums[i], i);
+//   }
+//   return false;
+// }
 
-    map.set(nums[i], i);
-  }
-  return false;
-}
+// console.log(containsDuplicate([ 1, 2, 3, 1 ])); // expected Ouput -> true
+// console.log(containsDuplicate([ 1, 2, 3, 4 ])); // expected Ouput -> false
+// console.log(containsDuplicate(
+//     [ 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 ])); // expected Ouput -> true
+
+// sorted
+// function containsDuplicate(nums: number[]): boolean
+// {
+//   nums.sort((a: number, b: number) => a - b);
+//
+//   for (let i: number = 1; i <= nums.length - 1; i++)
+//   {
+//     let j: number = i - 1;
+//     if (nums[i] === nums[j])
+//     {
+//       return true
+//     }
+//   }
+//   return false;
+// }
+
+// console.log(containsDuplicate([ 1, 2, 3, 1 ])); // expected Ouput -> true
+// console.log(containsDuplicate([ 1, 2, 3, 4 ])); // expected Ouput -> false
+// console.log(containsDuplicate([ 2, 14, 18, 22, 22 ])); // expected Ouput ->
+// true console.log(containsDuplicate(
+//     [ 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 ])); // expected Ouput -> true
+
+// brute Force
+
+// function containsDuplicate(nums: number[]): boolean
+// {
+//
+//   for (let i: number = 0; i <= nums.length - 1; i++)
+//   {
+//     for (let j: number = i + 1; j <= nums.length - 1; j++)
+//     {
+//       if (nums[i] === nums[j])
+//       {
+//         return true;
+//       }
+//     }
+//   }
+//
+//   return false
+// }
 
 console.log(containsDuplicate([ 1, 2, 3, 1 ])); // expected Ouput -> true
 console.log(containsDuplicate([ 1, 2, 3, 4 ])); // expected Ouput -> false
+console.log(containsDuplicate([ 2, 14, 18, 22, 22 ])); // expected Ouput -> true
 console.log(containsDuplicate(
     [ 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 ])); // expected Ouput -> true
