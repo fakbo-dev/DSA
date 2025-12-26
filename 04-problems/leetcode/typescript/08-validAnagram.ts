9e989c745e21b9235cc62c71ab0d1fb8ad78b849
fakbodev <fakbodev@gmail.com>
2025-12-26 11:02:44 -0400

    done with ValidAnagram

--- 04-problems/leetcode/typescript/08-validAnagram.ts
+++ 04-problems/leetcode/typescript/08-validAnagram.ts
@@ -18,27 +18,78 @@
 // console.log(isAnagram('anagram', 'nagaram'));
 // console.log(isAnagram('rat', 'car'));
 
+// METHOD 2 using hashmap
+// function isAnagram(s: string, t: string): boolean
+// {
+//   if (s.length !== t.length)
+//   {
+//     return false;
+//   }
+//   const firstMap: Map<string, number> = new Map();
+//   const secondMap: Map<string, number> = new Map();
+//
+//   for (const element of s)
+//   {
+//     if (!firstMap.has(element))
+//     {
+//       firstMap.set(element, 1);
+//     }
+//     else
+//     {
+//       firstMap.set(element, firstMap.get(element)! + 1);
+//     }
+//   }
+//   for (const element of t)
+//   {
+//     if (!secondMap.has(element))
+//     {
+//       secondMap.set(element, 1);
+//     }
+//     else
+//     {
+//       secondMap.set(element, secondMap.get(element)! + 1);
+//     }
+//   }
+//   for (const [key, value] of firstMap)
+//   {
+//     if (secondMap.get(key) !== value)
+//     {
+//       return false;
+//     }
+//   }
+//
+//   return true;
+// }
+// console.log(isAnagram('anagram', 'nagaram'));
+// console.log(isAnagram('rat', 'car'));
+
+// Method 3 using a arr to Count
+
 function isAnagram(s: string, t: string): boolean
 {
   if (s.length !== t.length)
   {
-    return false
+    return false;
   }
+  const firstArr: number[] = new Array(26).fill(0);
 
-  const map = new Map();
-
-  for (const elements of s)
+  for (let i = 0; i <= s.length - 1; i++)
   {
+    firstArr[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+  }
 
-    if (!map.has(elements))
+  for (let i = 0; i <= t.length - 1; i++)
+  {
+    firstArr[t.charCodeAt(i) - 'a'.charCodeAt(0)]--;
+  }
+  for (const element of firstArr)
+  {
+    if (element !== 0)
     {
-      map.set(elements, 1);
-    }
-    else
-    {
-      map.set(elements, map.get(elements) + 1);
+      return false;
     }
   }
+  return true;
 }
 
 console.log(isAnagram('anagram', 'nagaram'));
