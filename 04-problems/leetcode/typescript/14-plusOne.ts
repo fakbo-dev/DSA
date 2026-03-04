bfe6707823aea92e7c03188f5d02620460f080e7
fakbodev <fakbodev@gmail.com>
2026-03-04 12:02:36 -0400

    feat:add a new problem

---
+++ 04-problems/leetcode/typescript/14-plusOne.ts
@@ -0,0 +1,20 @@
+function plusOne(digits: number[]): number[]
+{
+  for (let i = digits.length - 1; i >= 0; i--)
+  {
+    if (digits[i] < 9)
+    {
+      digits[i]++;
+      return digits
+    }
+    digits[i] = 0;
+  }
+
+  return [ 1, ...digits ]
+};
+
+// console.log(plusOne([ 1, 2, 3 ]));
+// console.log(plusOne([ 4, 3, 2, 1 ]));
+// console.log(plusOne([ 9 ]));
+console.log(
+    plusOne([ 6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3 ]));
